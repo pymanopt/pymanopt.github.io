ca912895e042e3e9bd0505033e0f8155e0eda963
Jamie Townsend <jamiehntownsend@gmail.com>
2016-07-21 12:29:19 +0200

    Include __init__ methods in Sphinx doc.
    Also slight rewording of reference to Manifold abc in website.
    
    Signed-off-by: Jamie Townsend <jamiehntownsend@gmail.com>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -15,10 +15,19 @@
 import sys
 import os
 
+# Ensure that __init__ docstrings are included
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('/users/jamietownsend/pymanopt/'))
 
 # -- General configuration ------------------------------------------------
 
